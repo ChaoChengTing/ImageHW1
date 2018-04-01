f8b3f5b8f9d5866bb182fbaac4ced0b182289745
a104590036 <t104590036@ntut.org.tw>
2018-04-02 00:04:32 +0800

    OTSU

--- DigitalImageProcessing/imageProcessing.cs
+++ DigitalImageProcessing/imageProcessing.cs
@@ -61,12 +61,24 @@ namespace DigitalImageProcessing
         public static Image<Bgr, Byte> ConvertToOtsu(Image<Bgr, Byte> source)
         {
             Image<Bgr, Byte> result = new Image<Bgr, Byte>(source.Width, source.Height);
-            byte grayColor = 0;
-
+            int[,] grayColor = new int[256, 3];
+            byte gray = 0;
             int rows = source.Height;
             int cols = source.Width;
+            int totalPixel = rows * cols;
+            float meanAverage = 0, biggerAverage = 0, smallerAverage = 0;
+            float tempG = 0, finalG = 1, n0 = 0, n1 = 0, I0 = 0, I1 = 0;
+            float w0 = 0, w1 = 0;
 
+            byte t = 0, finalT = 0;
             byte r, g, b;
+
+            for (int i = 0; i < 256; i++)
+            {
+                grayColor[i, 0] = i;
+                grayColor[i, 1] = 0;
+            }
+
             for (int y = 0; y < rows; y++)
             {
                 for (int x = 0; x < cols; x++)
@@ -74,26 +86,72 @@ namespace DigitalImageProcessing
                     b = (byte)source.Data[y, x, 0];  //B
                     g = (byte)source.Data[y, x, 1];  //G
                     r = (byte)source.Data[y, x, 2];  //R
-                    grayColor = (byte)(b * 0.114 + g * 0.587 + r * 0.299);
-                    //average += (int)grayColor;
+                    gray = (byte)(b * 0.114 + g * 0.587 + r * 0.299);
+                    for (int i = 0; i < 256; i++)
+                    {
+                        if (gray == grayColor[i, 0]) grayColor[i, 1]++;
+                    }
+                    meanAverage += gray; //灰階總數
                 }
             }
-            /*average = average / (rows * cols);
-            for (int y = 0; y < rows; y++)
+            meanAverage = meanAverage / (rows * cols); //整體平均灰階數
+
+            for (int i = 0; i < 256; i++)
+            {
+                grayColor[i, 2] = grayColor[i, 0] * grayColor[i, 1];
+            }
+
+            for (t = 0; t < 255; t++)
+            {
+                n0 = 0;
+                n1 = 0;
+                I0 = 0;
+                I1 = 0;
+                for (int i = 0; i < 256; i++)
+                {
+                    if (grayColor[i, 0] < t)
+                    {
+                        n0 += grayColor[i, 1];
+                        I0 += grayColor[i, 2];
+                    }
+                    else
+                    {
+                        n1 += grayColor[i, 1];
+                        I1 += grayColor[i, 2];
+                    }
+                }
+                if (n0 == 0 || n1 == 0) continue;
+                else
+                {
+                    smallerAverage = I0 / n0;
+                    biggerAverage = I1 / n1;
+                    w0 = n0 / totalPixel;
+                    w1 = n1 / totalPixel;
+                    tempG = (int)(w0 * w1 * Math.Abs((biggerAverage - meanAverage) * (smallerAverage - meanAverage)));
+                    if (tempG > finalG)
+                    {
+                        finalG = tempG;
+                        finalT = t;
+                    }
+                }
+            }
+
+            for (int y = 0; y < rows; y++) //利用平均灰階計算二值化
             {
                 for (int x = 0; x < cols; x++)
                 {
                     b = (byte)source.Data[y, x, 0];  //B
                     g = (byte)source.Data[y, x, 1];  //G
                     r = (byte)source.Data[y, x, 2];  //R
-                    grayColor = (byte)(b * 0.114 + g * 0.587 + r * 0.299);
-                    if (grayColor <= average) grayColor = 0;
-                    else grayColor = 255;
-                    result.Data[y, x, 0] = grayColor;
-                    result.Data[y, x, 1] = grayColor;
-                    result.Data[y, x, 2] = grayColor;
+                    gray = (byte)(b * 0.114 + g * 0.587 + r * 0.299);
+                    if (gray <= finalT) gray = 0;
+                    else gray = 255;
+                    result.Data[y, x, 0] = gray;
+                    result.Data[y, x, 1] = gray;
+                    result.Data[y, x, 2] = gray;
                 }
-            }*/
+            }
+
             return result;
         }
 
