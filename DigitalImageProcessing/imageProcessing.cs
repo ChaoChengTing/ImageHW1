0d9ca6fc0ab0c8736a483db70369ddfb45fc8afa
a104590036 <t104590036@ntut.org.tw>
2018-04-01 21:06:01 +0800

    test OTSU

--- DigitalImageProcessing/imageProcessing.cs
+++ DigitalImageProcessing/imageProcessing.cs
@@ -61,7 +61,39 @@ namespace DigitalImageProcessing
         public static Image<Bgr, Byte> ConvertToOtsu(Image<Bgr, Byte> source)
         {
             Image<Bgr, Byte> result = new Image<Bgr, Byte>(source.Width, source.Height);
+            byte grayColor = 0;
 
+            int rows = source.Height;
+            int cols = source.Width;
+
+            byte r, g, b;
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    b = (byte)source.Data[y, x, 0];  //B
+                    g = (byte)source.Data[y, x, 1];  //G
+                    r = (byte)source.Data[y, x, 2];  //R
+                    grayColor = (byte)(b * 0.114 + g * 0.587 + r * 0.299);
+                    //average += (int)grayColor;
+                }
+            }
+            /*average = average / (rows * cols);
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    b = (byte)source.Data[y, x, 0];  //B
+                    g = (byte)source.Data[y, x, 1];  //G
+                    r = (byte)source.Data[y, x, 2];  //R
+                    grayColor = (byte)(b * 0.114 + g * 0.587 + r * 0.299);
+                    if (grayColor <= average) grayColor = 0;
+                    else grayColor = 255;
+                    result.Data[y, x, 0] = grayColor;
+                    result.Data[y, x, 1] = grayColor;
+                    result.Data[y, x, 2] = grayColor;
+                }
+            }*/
             return result;
         }
 
