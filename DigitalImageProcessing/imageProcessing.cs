7f93a0e659ea027dbedce22ec591f970077a19e6
a104590036 <t104590036@ntut.org.tw>
2018-04-01 21:04:30 +0800

    Initial

---
+++ DigitalImageProcessing/imageProcessing.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+using System.Runtime.InteropServices;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace DigitalImageProcessing
+{
+    public static class imageProcessing
+    {
+        public static Image<Bgr, Byte> ConvertToGray(Image<Bgr, Byte> source)  //灰階 function //Gray = R*0.299 + G*0.587 + B*0.114
+        {
+            Image<Bgr, Byte> result = new Image<Bgr, Byte>(source.Width, source.Height);
+
+            int rows = source.Height;
+            int cols = source.Width;
+
+            byte r, g, b;
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    b = (byte)source.Data[y, x, 0];  //B
+                    g = (byte)source.Data[y, x, 1];  //G
+                    r = (byte)source.Data[y, x, 2];  //R
+
+                    byte grayColor = (byte)(b * 0.114 + g * 0.587 + r * 0.299);
+                    result.Data[y, x, 0] = grayColor;
+                    result.Data[y, x, 1] = grayColor;
+                    result.Data[y, x, 2] = grayColor;
+                }
+            }
+
+            return result;
+        }
+
+        public static Image<Bgr, Byte> ConvertToMirror(Image<Bgr, Byte> source)
+        {
+            Image<Bgr, Byte> result = new Image<Bgr, Byte>(source.Width, source.Height);
+
+            int rows = source.Height;
+            int cols = source.Width;
+
+            byte r, g, b;
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    b = (byte)source.Data[y, x, 0];  //B
+                    g = (byte)source.Data[y, x, 1];  //G
+                    r = (byte)source.Data[y, x, 2];  //R
+
+                    result.Data[y, (cols - x - 1), 0] = b;
+                    result.Data[y, (cols - x - 1), 1] = g;
+                    result.Data[y, (cols - x - 1), 2] = r;
+                }
+            }
+            return result;
+        }
+
+        public static Image<Bgr, Byte> ConvertToOtsu(Image<Bgr, Byte> source)
+        {
+            Image<Bgr, Byte> result = new Image<Bgr, Byte>(source.Width, source.Height);
+
+            return result;
+        }
+
+        public static Image<Bgr, Byte> HistogramEqualization(Image<Bgr, Byte> source)
+        {
+            Image<Bgr, Byte> result = new Image<Bgr, Byte>(source.Width, source.Height);
+
+            return result;
+        }
+
+        public static Image<Bgr, Byte> imageBlending(Image<Bgr, Byte> source, Image<Bgr, Byte> source2, double Threshold)
+        {
+            Image<Bgr, Byte> result = new Image<Bgr, Byte>(source.Width, source.Height);
+
+            return result;
+        }
+
+        public static Image<Bgr, Byte> Rotating(Image<Bgr, Byte> source, double theta)
+        {
+            Image<Bgr, Byte> result = new Image<Bgr, Byte>(source.Width, source.Height);
+
+            return result;
+        }
+    }
+}
